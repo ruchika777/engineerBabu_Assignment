ff8d176e69e8fd8d05177b8661d6e0e20ed74ce6
ruchika.pawar <ruchikapawar7@gmail.com>
2021-08-26 23:04:31 +0530

    initial commit

---
+++ server/bin/www.js
@@ -0,0 +1,28 @@
+var app = require('../app');
+var debug = require('debug');
+var http = require('http');
+let config = require('../config');
+
+/**
+ * Get port from environment and store in Express.
+ */
+
+app.set('env', config.NODE_ENV);
+app.set('port', process.env.PORT || 3000);
+
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen( app.get('port'), '0.0.0.0', () => {
+  console.log('Server is up and running on port number '+ app.get('port'));
+});
+
+
